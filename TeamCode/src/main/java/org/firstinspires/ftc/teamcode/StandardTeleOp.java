cebdd8d431d6d778814f39b09ef796bbcacffd5a
AJPortis <aaronjportis@gmail.com>
2024-10-17 21:06:55 +0100

    Adjusted my adjustments from earlier.... Also added telemetry.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StandardTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StandardTeleOp.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2022 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -37,37 +8,6 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.RobotHardware;
-
-/*
- * This OpMode illustrates how to use an external "hardware" class to modularize all the robot's sensors and actuators.
- * This approach is very efficient because the same hardware class can be used by all of your teleop and autonomous OpModes
- * without requiring many copy & paste operations.  Once you have defined and tested the hardware class with one OpMode,
- * it is instantly available to other OpModes.
- *
- * The real benefit of this approach is that as you tweak your robot hardware, you only need to make changes in ONE place (the Hardware Class).
- * So, to be effective you should put as much or your hardware setup and access code as possible in the hardware class.
- * Essentially anything you do with hardware in BOTH Teleop and Auto should likely go in the hardware class.
- *
- * The Hardware Class is created in a separate file, and then an "instance" of this class is created in each OpMode.
- * In order for the class to do typical OpMode things (like send telemetry data) it must be passed a reference to the
- * OpMode object when it's created, so it can access all core OpMode functions.  This is illustrated below.
- *
- * In this concept sample, the hardware class file is called RobotHardware.java and it must accompany this sample OpMode.
- * So, if you copy StandardTeleOp.java into TeamCode (using Android Studio or OnBotJava) then RobotHardware.java
- * must also be copied to the same location (maintaining its name).
- *
- * For comparison purposes, this sample and its accompanying hardware class duplicates the functionality of the
- * RobotTelopPOV_Linear OpMode.  It assumes three motors (left_drive, right_drive and arm) and two servos (left_hand and right_hand)
- *
- * View the RobotHardware.java class file for more details
- *
- *  Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- *  Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- *
- *  In OnBot Java, add a new OpMode, select this sample, and select TeleOp.
- *  Also add another new file named RobotHardware.java, select the sample with that name, and select Not an OpMode.
- */
-
 @TeleOp(name="StandardTeleOp", group="Robot")
 public class StandardTeleOp extends LinearOpMode {
 
@@ -78,14 +18,6 @@ public class StandardTeleOp extends LinearOpMode {
     @Override
     public void runOpMode() {
         double slowmodeMult;
-
-//        DcMotor frontLeft = hardwareMap.dcMotor.get("front_Left");
-//        DcMotor backLeft = hardwareMap.dcMotor.get("back_Left");
-//        DcMotor frontRight = hardwareMap.dcMotor.get("front_Right");
-//        DcMotor backRight = hardwareMap.dcMotor.get("back_Right");
-
-//        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-//        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
         // initialize all the hardware, using the hardware class. See how clean and simple this is?
         robot.init();
 
@@ -102,7 +34,7 @@ public class StandardTeleOp extends LinearOpMode {
                 slowmodeMult = 0.4;
             }
             else{
-                slowmodeMult = 1;
+                slowmodeMult = 0.8;
             }
 
             double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
@@ -123,7 +55,7 @@ public class StandardTeleOp extends LinearOpMode {
 
             frontLeftPower *= slowmodeMult;
             backLeftPower *= slowmodeMult;
-            frontRightPower *= slowmodeMult * 1.05;
+            frontRightPower *= slowmodeMult * 1.05;//TWEAK THIS VALUE!!! LESS!!!
             backRightPower *= slowmodeMult;
 
 
@@ -133,23 +65,27 @@ public class StandardTeleOp extends LinearOpMode {
             robot.backRight.setPower(backRightPower);
 
             //Arm
+
             if(gamepad2.square){
+                robot.intakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+                robot.intakeMotor.setPower(0.6);
+            }
+            else if(gamepad2.cross){
+                robot.intakeMotor.setDirection(DcMotorSimple.Direction.FORWARD);
                 robot.intakeMotor.setPower(0.6);
             }
             else{
                 robot.intakeMotor.setPower(0);
             }
 
-            if(gamepad2.circle){
-                robot.intakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-            }
-            if(gamepad2.triangle){
-                robot.intakeMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-            }
-
             // Send telemetry messages to explain controls and show robot status
-            telemetry.addData("Arm Up/Down", "Y & A Buttons");
-            telemetry.addData("Hand Open/Closed", "Left and Right Bumpers");
+            telemetry.addData("GAMEPAD 1 LEFT STICK Y", -gamepad1.left_stick_y);
+            telemetry.addData("GAMEPAD 1 LEFT STICK X", gamepad1.left_stick_x);
+            telemetry.addData("GAMEPAD 1 RIGHT STICK X", gamepad1.right_stick_x);
+            telemetry.addData("MOTOR STATUS", robot.backLeft.isBusy() && robot.frontLeft.isBusy() && robot.frontRight.isBusy() && robot.backRight.isBusy());
+            telemetry.addData("--------------------------------------------------", "");
+            telemetry.addData("INTAKE POWER", robot.intakeMotor.getPower());
+            telemetry.addData("INTAKE DIRECTION", robot.intakeMotor.getDirection());
             telemetry.update();
 
             // Pace this loop so hands move at a reasonable speed.
